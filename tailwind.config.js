728c8865bb97478863fb00c9e25816e527b8223f
Pavel159 <general9dev@gmail.com>
2023-04-04 03:09:24 +0300

    font fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,5 @@ const withMT = require('@material-tailwind/react/utils/withMT');
 
 module.exports = withMT({
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        Roboto: ["Manrope', sans-serif"],
-      },
-    },
-  },
   plugins: [],
 });
